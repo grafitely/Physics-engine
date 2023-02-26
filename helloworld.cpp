2d13657b84c28fb93dfa848fb4b3a95406c51b22
grafitely <oliver.carruthers@outlook.com>
2023-02-26 16:36:23 +0000

    i have to enter one of these every time

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 string name;
 
+string firstletter;
+
 int main() {
   cout << "Welcome what is your name?";
   cin >> name;
-  char firstletter = name[0];
+  firstletter = name[0];
   cout << "Hello There!, " + name + ", The first letter of your name is \"" + firstletter + "\"";
   return 0;
 }
