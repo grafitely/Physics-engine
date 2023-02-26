163872e5018d07afd58a0219e45859622910e267
grafitely <oliver.carruthers@outlook.com>
2023-02-26 16:33:02 +0000

    Hello world

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 string name;
 int main() {
+  cout << "Welcome what is your name?";
   cin >> name;
-  cout << "Hello World!, " + name;
+  char firstletter = name[0];
+  cout << "Hello There!, " + name + ", The first letter of your name is \"" + firstletter + "\"";
   return 0;
 }
